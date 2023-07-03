95805910db12cc79fda365cf881eb299d282f11e
szczynk <34181070+szczynk@users.noreply.github.com>
2023-07-03 19:28:02 +0700

    feat: LinearSearchList

---
+++ src/day1/LinearSearchList.go
@@ -0,0 +1,10 @@
+package main
+
+func linear_search(haystack []int, needle int) bool {
+	for i := 0; i < len(haystack); i++ {
+		if haystack[i] == needle {
+			return true
+		}
+	}
+	return false
+}
---
+++ src/day1/LinearSearchList_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func Test_linear_search(t *testing.T) {
+	type args struct {
+		haystack []int
+		needle   int
+	}
+	foo := []int{1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "find 69",
+			args: args{haystack: foo, needle: 69},
+			want: true,
+		},
+		{
+			name: "find 1336, not found",
+			args: args{haystack: foo, needle: 1336},
+			want: false,
+		},
+		{
+			name: "find 69420",
+			args: args{haystack: foo, needle: 69420},
+			want: true,
+		},
+		{
+			name: "find 69421, not found",
+			args: args{haystack: foo, needle: 69421},
+			want: false,
+		},
+		{
+			name: "find 1",
+			args: args{haystack: foo, needle: 1},
+			want: true,
+		},
+		{
+			name: "find 0, not found",
+			args: args{haystack: foo, needle: 0},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := linear_search(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("linear_search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
